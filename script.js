849390da33b692c13af84581ec753b7faa708249
umar0055 <umarsohail648@gmail.com>
2025-01-08 15:00:33 +0500

    assigment no 9

---
+++ script.js
@@ -0,0 +1,52 @@
+// assigement 1
+
+// var natural = 0;
+// while (natural <= 100) {
+//     console.log(natural);
+//     natural++;
+// }
+
+// assigement 2
+// var natural = 0;
+// while (natural >= -100) {
+//     console.log(natural);
+//     natural--;
+// }
+
+// assigement 4
+// var even = "0";
+// while (even <=100  ) {
+//     if(even %2 ==0)
+//     console.log(even);
+//     even++;
+// }
+
+// assigement 5
+// var odd = "0";
+// while (odd <=100  ) {
+//     if(odd %2 ==1)
+//     console.log(odd);
+//     odd++;
+// }
+
+//assigement 7
+// var count = 1;
+// var sum = 0;
+// while(count <= 100){
+//     if(count % 2 == 0){
+//         sum = sum + count;
+//     }
+//     count++;
+// }
+// console.log( sum);
+
+// assigement 8
+// var count = 1;
+// var sum = 0;
+// while(count <= 100){
+//     if(count % 2 == 1){
+//         sum = sum + count;
+//     }
+//     count++;
+// }
+// console.log( sum);
