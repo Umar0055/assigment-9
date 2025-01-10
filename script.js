6178c6d5a97668b00112e6931cac009ba6058b05
umar0055 <umarsohail648@gmail.com>
2025-01-10 15:55:31 +0500

    loops

--- script.js
+++ script.js
@@ -104,3 +104,71 @@
 // console.log("Last digit: " + lastDigit);
 // var sum = firstDigit + lastDigit;
 // console.log("Sum of first and last digit: " + sum);
+
+
+
+
+
+//assigement 16
+// var number =12345;
+// var resverdnum =parseInt(number.toString().split('').reverse().join(''));
+//  console.log(resverdnum);
+
+// assigement 17
+// var word = "lol";
+// var reverseWord = "";
+// for (var i = word.length - 1; i >= 0; i--) {
+//     reverseWord += word[i];
+// }
+// if (
+//     word.toLowerCase() === reverseWord.toLowerCase()
+// ) {
+//     console.log("The word is a palindrome.");
+// } else {
+//     console.log("The word is not a palindrome.");
+// }
+
+  
+    
+
+
+                       
+
+// assigement 19
+// var number = 1;
+// var digit;
+// while (number > 0) {
+// digit = number % 10;
+// number = Math.floor(number / 10);
+// if(digit == 0){
+//     console.log("zero");
+// }
+// else if(digit == 1){
+//     console.log("one");
+// }
+// else if(digit == 2){
+//     console.log("two");
+// }
+// else if(digit == 3){
+//     console.log("three");
+// }
+// else if(digit == 4){
+//     console.log("four");
+// }
+// else if(digit == 5){
+//     console.log("five");
+// }
+// else if(digit == 6){
+//     console.log("six");
+// }
+// else if(digit == 7){
+//     console.log("seven");
+// }
+// else if(digit == 8){
+//     console.log("eight");
+// }
+// else if(digit == 9){
+//     console.log("nine");
+// } else {
+//     console.log("Invalid digit");
+// }}
